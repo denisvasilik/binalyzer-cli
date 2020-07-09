7c1ddcdddd4f2804671a82e9b514a3f7fee64665
Denis Vasilik <contact@denisvasilik.com>
2020-07-09 20:04:10 +0200

    Add version option to BinalyzerGroup

--- binalyzer_cli/cli.py
+++ binalyzer_cli/cli.py
@@ -1,7 +1,8 @@
 import os
 import click
-
+import importlib
 import hexdump
+import pkg_resources
 
 from anytree import Resolver
 from anytree.resolver import ResolverError
@@ -16,6 +17,48 @@ from binalyzer import (
     __version__,
     BufferedIODataProvider,
 )
+
+_BINALYZER_PACKAGES = [
+    "binalyzer",
+    "binalyzer_core",
+    "binalyzer_cli",
+    "binalyzer_data_provider",
+    "binalyzer_template_provider",
+]
+
+
+def print_version(ctx, _param, value):
+    if not value or ctx.resilient_parsing:
+        return
+
+    for package_name in _BINALYZER_PACKAGES:
+        try_print_version_info(package_name, ctx)
+
+    extension_packages = []
+    for ep in pkg_resources.iter_entry_points("binalyzer.commands"):
+        if not package_name in _BINALYZER_PACKAGES:
+            package_name = ep.module_name.split(".")[0]
+            extension_packages.append(package_name)
+
+    for package_name in extension_packages:
+        try_print_version_info(package_name, ctx)
+
+    ctx.exit()
+
+
+def try_print_version_info(package_name, ctx):
+    click.echo(try_get_version_info(package_name), color=ctx.color)
+
+
+def try_get_version_info(package_name):
+    try:
+        package = importlib.import_module(package_name)
+        if package.__version__:
+            return "{:s} ({:s})".format(package_name, package.__version__[1:])
+        else:
+            return "{:s} ({:s})".format(package_name, package.__commit__[:7])
+    except ImportError:
+        return "{:s} not installed".format(package_name)
 
 
 class BasedIntParamType(click.ParamType):
@@ -112,8 +155,17 @@ class BinalyzerGroup(click.Group):
     ):
         params = list(extra.pop("params", None) or ())
 
-        if not version_option is None:
-            params.append(version_option)
+        if version_option is None:
+            version_option = click.Option(
+                ["--version"],
+                help="Show the Binalyzer version",
+                expose_value=False,
+                callback=print_version,
+                is_flag=True,
+                is_eager=True,
+            )
+
+        params.append(version_option)
 
         click.Group.__init__(self, params=params, **extra)
         self._loaded_plugin_commands = False
